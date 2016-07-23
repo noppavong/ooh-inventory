ccaa8e32dc555b491a4cc59906b9c8add9541b3b
noppavong <root@MacBook-Air-khxng-au.local>
2016-07-23 14:54:47 +0700

    change database.config
    xxxxx

--- application/config/database.php
+++ application/config/database.php
@@ -45,25 +45,39 @@
 | the active record class
 */
 
+
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'sql110.byethost12.com';
-$db['default']['username'] = 'b12_18575330';
-$db['default']['password'] = 'cf3uhe4x';
-$db['default']['database'] = 'b12_18575330_inventory';
-$db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
 $db['default']['db_debug'] = TRUE;
 $db['default']['cache_on'] = FALSE;
-$db['default']['cachedir'] = '';
+$db['default']['cachedir'] = getenv('OPENSHIFT_DATA_DIR') . 'storage/framework/dbcache';
 $db['default']['char_set'] = 'utf8';
 $db['default']['dbcollat'] = 'utf8_general_ci';
 $db['default']['swap_pre'] = '';
 $db['default']['autoinit'] = TRUE;
 $db['default']['stricton'] = FALSE;
 
+if(getenv('OPENSHIFT_MYSQL_DB_HOST'))
+{
+	$db['default']['dbdriver'] = 'mysqli';
+	$db['default']['hostname'] = getenv('OPENSHIFT_MYSQL_DB_HOST');
+	$db['default']['port'] = getenv('OPENSHIFT_MYSQL_DB_PORT');
+	$db['default']['username'] = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+	$db['default']['password'] = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+	$db['default']['database'] = getenv('OPENSHIFT_APP_NAME');	
+}
+else
+{
+	$db['default']['dbdriver'] = 'postgre';
+	$db['default']['hostname'] = getenv('OPENSHIFT_POSTGRESQL_DB_HOST');
+	$db['default']['port'] = getenv('OPENSHIFT_POSTGRESQL_DB_PORT');
+	$db['default']['username'] = getenv('OPENSHIFT_POSTGRESQL_DB_USERNAME');
+	$db['default']['password'] = getenv('OPENSHIFT_POSTGRESQL_DB_PASSWORD');
+	$db['default']['database'] = getenv('OPENSHIFT_APP_NAME');
+}
 
 /* End of file database.php */
 /* Location: ./application/config/database.php */
